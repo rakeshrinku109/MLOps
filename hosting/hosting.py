7c4c3b987fed48be4628a5c01efd380dedd006c4
RakeshPadhi <rakesh.rinku109@gmail.com>
2026-03-22 19:01:32 +0530

    Update hosting.py

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HUGGINGFACE_ACCESS_KEY"))
 api.upload_folder(
-    folder_path="mlops/deployment",     # the local folder containing your files
+    folder_path="deployment",     # the local folder containing your files
     repo_id="rkpworks/Bank-Customer-Churn",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
