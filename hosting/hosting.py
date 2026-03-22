124dc4aeb8cb812d498d179c4e9588ac11e80863
rakeshrinku109 <rakesh.rinku109@gmail.com>
2026-03-22 12:49:07 +0000

    Cleaned repository and added fresh MachineLearningOps project files

---
+++ hosting/hosting.py
@@ -0,0 +1,10 @@
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HUGGINGFACE_ACCESS_KEY"))
+api.upload_folder(
+    folder_path="mlops/deployment",     # the local folder containing your files
+    repo_id="rkpworks/Bank-Customer-Churn",          # the target repo
+    repo_type="space",                      # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
